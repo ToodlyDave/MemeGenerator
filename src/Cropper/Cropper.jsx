7e39e729ca1687d0c59b7636b438ceb7f37e71ce
Jacob1248 <jacobchacko228@gmail.com>
2020-11-10 20:55:08 +0300

    Shortened the code for the cropper element

--- src/Cropper/Cropper.jsx
+++ src/Cropper/Cropper.jsx
@@ -15,10 +15,7 @@ export const Cropper = ({file}) =>{
 
     let [imageDims,setImageDims] = useState({
         width:0,
-        height:0,
-        left:0,
-        top:0,
-        scale:1,
+        height:0
     })
 
     useEffect(() => {
@@ -36,10 +33,7 @@ export const Cropper = ({file}) =>{
     //this component also sets the cropper box initially
     const setDims = () =>{
         if(imageRef.current && holderRef.current){
-            let temp = holderRef.current.getBoundingClientRect()
-            let scaleX =  holderRef.current.offsetWidth/imageRef.current.offsetWidth
-            let scaleY =  holderRef.current.offsetHeight/imageRef.current.offsetHeight
-            let scale = Math.min(scaleX,scaleY)
+            let scale = Math.min(holderRef.current.offsetWidth/imageRef.current.offsetWidth, holderRef.current.offsetHeight/imageRef.current.offsetHeight)
             setCropperDims((cropperDim)=>{
                 return {
                 width:cropperDim.width==0?imageRef.current.offsetWidth*scale:cropperDim.width/cropperDim.scale*scale,
@@ -52,87 +46,95 @@ export const Cropper = ({file}) =>{
             setImageDims(()=>{
                 return {
                     width:imageRef.current.offsetWidth*scale,
-                    height:imageRef.current.offsetHeight*scale,
-                    left:0,
-                    top:0,
+                    height:imageRef.current.offsetHeight*scale
                 }
             })
         }
     }
 
+    const leftPingX = (clientX,temp,temp1) =>{
+        if(clientX >= 0 && clientX <= temp.right -temp1.left -10 && clientX <=0 + imageDims.width -10){
+            let leftDifference = clientX - cropperDim.left
+            let widthDifference = cropperDim.width - leftDifference
+            return {leftDifference:leftDifference,widthDifference:widthDifference}
+        }
+        return {leftDifference:0,widthDifference:0}
+    }
+
+    const rightPingX = (clientX,temp,temp1) =>{
+        if(clientX <= 0 + imageDims.width && clientX >= temp.left - temp1.left +10 && clientX >= 0 + 10){
+            let leftDifference = clientX - cropperDim.left - cropperDim.width
+            let widthDifference = cropperDim.width + leftDifference
+            return {widthDifference:widthDifference}
+        }
+        return {widthDifference:0}
+    }
+
+    const topPingY = (clientY,temp,temp1) =>{
+        if(clientY >= 0 && clientY <= temp.top + temp.height - temp1.top -10){
+            let topDifference = clientY - cropperDim.top
+            let heightDifference = (cropperDim.height - topDifference)<imageDims.height?cropperDim.height - topDifference:imageDims.height
+            return {topDifference:topDifference,heightDifference:heightDifference}
+        }
+        return {topDifference:0,heightDifference:0}
+    }
+
+    const bottomPingY = (clientY,temp,temp1) =>{
+        if(clientY <= 0 + imageDims.height && clientY >=  temp.top - temp1.top + 10){
+            let topDifference = clientY - cropperDim.top - cropperDim.height
+            let heightDifference = (cropperDim.height + topDifference)<0 + imageDims.height +10?cropperDim.height + topDifference:imageDims.height + 0
+            return {topDifference:topDifference,heightDifference:heightDifference}
+        }
+        return {topDifference:0,heightDifference:0}
+    }
+
+    const returnMousePosition = (event,holderRef) =>{
+        return {clientX:event.clientX - holderRef.left,clientY:event.clientY - holderRef.top}
+    }
+
     const resize = (e) =>{
-        let temp = cropRef.getBoundingClientRect()
-        let temp1 = holderRef.current.getBoundingClientRect()
-        var clientX = e.clientX - temp1.left; //x position within the element.
-        var clientY = e.clientY - temp1.top;  //y position within the element.
-        let leftDifference = 0
-        let widthDifference = 0
-        let topDifference = 0
-        let heightDifference = 0
+        let [temp,temp1] = [cropRef.getBoundingClientRect(),holderRef.current.getBoundingClientRect()]
+        let {clientX,clientY} = returnMousePosition(e,temp1)
         switch(clicked){
             case 1:{
-                if(clientX >= imageDims.left && clientX <= temp.right -temp1.left -10 && clientX <=imageDims.left + imageDims.width -10){
-                    leftDifference = clientX - cropperDim.left
-                    widthDifference = cropperDim.width - leftDifference
-                }
-                if(clientY >= imageDims.top && clientY <= temp.top + temp.height - temp1.top -10){
-                    topDifference = clientY - cropperDim.top
-                    heightDifference = (cropperDim.height - topDifference)<imageDims.height?cropperDim.height - topDifference:imageDims.height
-                }
+                let {leftDifference,widthDifference} = leftPingX(clientX,temp,temp1)
+                let {topDifference,heightDifference} = topPingY(clientY,temp,temp1)
                 setCropperDims({
                     ...cropperDim,
                     height:(heightDifference>0)?heightDifference:cropperDim.height,
-                    top:cropperDim.top + topDifference > imageDims.top?cropperDim.top + topDifference:cropperDim.top,
-                    left: (cropperDim.left + leftDifference)>imageDims.left?cropperDim.left + leftDifference:imageDims.left,
+                    top:cropperDim.top + topDifference > 0?cropperDim.top + topDifference:cropperDim.top,
+                    left: (cropperDim.left + leftDifference)>0?cropperDim.left + leftDifference:0,
                     width:(widthDifference>0)?widthDifference:cropperDim.width,
                 })
                 return
             }
             case 2:{
-                if(clientX <= imageDims.left + imageDims.width && clientX >= temp.left - temp1.left +10 && clientX >= imageDims.left + 10){
-                    leftDifference = clientX - cropperDim.left - cropperDim.width
-                    widthDifference = cropperDim.width + leftDifference 
-                }
-                if(clientY >= imageDims.top && clientY <= temp.top + temp.height - temp1.top -10){
-                    topDifference = clientY - cropperDim.top
-                    heightDifference = (cropperDim.height - topDifference)<imageDims.height?cropperDim.height - topDifference:imageDims.height
-                }
+                let {widthDifference} = rightPingX(clientX,temp,temp1)
+                let {topDifference,heightDifference} = topPingY(clientY,temp,temp1)
                 setCropperDims({
                     ...cropperDim,
                     height:(heightDifference>0)?heightDifference:cropperDim.height,
-                    top:cropperDim.top + topDifference > imageDims.top?cropperDim.top + topDifference:cropperDim.top,
+                    top:cropperDim.top + topDifference > 0?cropperDim.top + topDifference:cropperDim.top,
                     left: cropperDim.left,
                     width:(widthDifference>0)?widthDifference:cropperDim.width
                 })
                 return
             }
             case 3:{
-                if(clientX >= imageDims.left && clientX <= temp.right-temp1.left - 10 && clientX <=imageDims.left + imageDims.width -10){
-                    leftDifference = clientX - cropperDim.left
-                    widthDifference = cropperDim.width - leftDifference
-                }
-                if(clientY <= imageDims.top + imageDims.height && clientY >=  temp.top - temp1.top + 10){
-                    topDifference = clientY - cropperDim.top - cropperDim.height
-                    heightDifference = (cropperDim.height + topDifference)<imageDims.top + imageDims.height +10?cropperDim.height + topDifference:imageDims.height + imageDims.top
-                }
+                let {leftDifference,widthDifference} = leftPingX(clientX,temp,temp1)
+                let {heightDifference} = bottomPingY(clientY,temp,temp1)
                 setCropperDims({
                     ...cropperDim,
                     height:(heightDifference>0)?heightDifference:cropperDim.height,
                     top:cropperDim.top,
-                    left: (cropperDim.left + leftDifference)>imageDims.left?cropperDim.left + leftDifference:imageDims.left,
+                    left: (cropperDim.left + leftDifference)>0?cropperDim.left + leftDifference:0,
                     width:(widthDifference>0)?widthDifference:cropperDim.width
                 })
                 return
             }
             case 4:{
-                if(clientX <= imageDims.left + imageDims.width && clientX >= temp.left - temp1.left +10 && clientX >= imageDims.left + 10){
-                    leftDifference = clientX - cropperDim.left - cropperDim.width
-                    widthDifference = cropperDim.width + leftDifference 
-                }
-                if(clientY <= imageDims.top + imageDims.height && clientY >= temp.top - temp1.top + 10){
-                    topDifference = clientY - cropperDim.top - cropperDim.height
-                    heightDifference = (cropperDim.height + topDifference)<imageDims.top + imageDims.height +10?cropperDim.height + topDifference:imageDims.height + imageDims.top
-                }
+                let {widthDifference} = rightPingX(clientX,temp,temp1)
+                let {heightDifference} = bottomPingY(clientY,temp,temp1)
                 setCropperDims({
                     ...cropperDim,
                     height:(heightDifference>0)?heightDifference:cropperDim.height,
@@ -146,13 +148,8 @@ export const Cropper = ({file}) =>{
                 return
         }
     }
-
-    const mouseLeave = () =>{
-        setClick(0);
-    }
-
     return(
-        <div onLoad={setDims} className="holder" onMouseMove={(e)=>{resize(e)}} onMouseLeave={mouseLeave} onMouseUp={()=>{setClick(0)}} ref={holderRef}>
+        <div onLoad={setDims} className="holder" onMouseMove={(e)=>{resize(e)}} onMouseLeave={()=>{setClick(0);}} onMouseUp={()=>{setClick(0)}} ref={holderRef}>
             <div style={{width:cropperDim.width,height:cropperDim.height,top:cropperDim.top,left:cropperDim.left  }} ref={ref=>cropRef=ref} className="crop-rect">
                 <div onMouseDown={()=>{setClick(1)}} className="ping ping-top-left"></div>
                 <div onMouseDown={()=>{setClick(2)}} className="ping ping-top-right"></div>
@@ -160,10 +157,10 @@ export const Cropper = ({file}) =>{
                 <div onMouseDown={()=>{setClick(4)}} className="ping ping-bottom-right"></div>
             </div>
             <img alt="crop" ref={imageRef} style={{transform:`scale(${(cropperDim.scale)})`,top:0,left:0}} className="image-area" src={file}></img>
-            <div style={{height: cropperDim.top - imageDims.top,top:imageDims.top,left: imageDims.left,width:imageDims.width}} className="gray gray-top"></div>
-            <div style={{width:cropperDim.left - imageDims.left,top:cropperDim.top,left:imageDims.left,height:cropperDim.height}} className="gray gray-left"></div>
-            <div style={{width:imageDims.left+imageDims.width - cropperDim.left - cropperDim.width,top:cropperDim.top,left:cropperDim.left+cropperDim.width,height:cropperDim.height}} className="gray gray-right"></div>
-            <div style={{height:imageDims.top+imageDims.height-cropperDim.top-cropperDim.height,top:cropperDim.top+cropperDim.height,left:imageDims.left,width:imageDims.width}} className="gray gray-bottom"></div>
+            <div style={{height: cropperDim.top,top:0,left: 0,width:imageDims.width}} className="gray gray-top"></div>
+            <div style={{width:cropperDim.left,top:cropperDim.top,left:0,height:cropperDim.height}} className="gray gray-left"></div>
+            <div style={{width:imageDims.width - cropperDim.left - cropperDim.width,top:cropperDim.top,left:cropperDim.left+cropperDim.width,height:cropperDim.height}} className="gray gray-right"></div>
+            <div style={{height:imageDims.height-cropperDim.top-cropperDim.height,top:cropperDim.top+cropperDim.height,left:0,width:imageDims.width}} className="gray gray-bottom"></div>
         </div>
     )
 }
